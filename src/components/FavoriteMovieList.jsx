ebb55cc83c855c2315d6d737f904e4483a94dc39
emrahkiziltan94 <emrahkiziltan94@gmail.com>
2024-07-01 21:32:04 +0300

    dispatch issue solved.

--- src/components/FavoriteMovieList.jsx
+++ src/components/FavoriteMovieList.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 const FavoriteMovieList = (props) => {
+  const dispatch = useDispatch();
   const favorites = useSelector((store) => store.favoritesReducer.favorites);
 
   return (
